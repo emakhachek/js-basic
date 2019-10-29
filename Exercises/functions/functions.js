db60a5cd67f0b51d82159f4c291a331c2d99f0ae
Ekaterina Makhachek <maek@ciklum.com>
2019-10-29 11:56:39 +0200

    HomeWork was made partially

--- Exercises/functions/functions.js
+++ Exercises/functions/functions.js
@@ -1,3 +1,21 @@
-function takeMyParametersAmount(a, b, c) {
+// Task #1
+function functionDeclaration() {
   return null;
 }
+
+let functionExpression = function () { return null; };
+
+// Task #2
+function takeMyParametersAmount() {
+  let i = 0;
+  for (let arg of arguments)
+    i += 1;
+  console.log(i);
+  return null;
+}
+
+// Task #3
+
+
+// Task #4
+(function () { })();
