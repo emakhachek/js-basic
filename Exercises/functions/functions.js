2fe55f57e3222cbf02c8068bdb5132b334d278dc
Ekaterina Makhachek <maek@ciklum.com>
2019-10-30 19:12:44 +0200

    functions tasks are completed

--- Exercises/functions/functions.js
+++ Exercises/functions/functions.js
@@ -1,9 +1,13 @@
 // Task #1
-function functionDeclaration() {
+function functionDeclaration(a, b, ...rest) {
   return null;
 }
 
-let functionExpression = function () { return null; };
+const functionExpression = function (a, b, ...rest) { return null; };
+
+const functionObject = new Function ('a', 'b', 'return null');
+
+const arrowFunction = (a, b, ...rest) => null;
 
 // Task #2
 function takeMyParametersAmount() {
@@ -13,9 +17,15 @@ function takeMyParametersAmount() {
   console.log(i);
   return null;
 }
+takeMyParametersAmount('a','b','c');
 
 // Task #3
+function logPassedParams() {
+  return arguments.length;
+}
 
+let count = logPassedParams(1, 2, 3);
+console.log(count);
 
 // Task #4
 (function () { })();
