ca6ca2ff880ca373cf4242b26f848bf7c6b6aa7d
Ekaterina Makhachek <maek@ciklum.com>
2019-11-03 18:40:29 +0200

    Fixed task #2, #3, #4

--- Exercises/functions/functions.js
+++ Exercises/functions/functions.js
@@ -5,27 +5,24 @@ function functionDeclaration(a, b, ...rest) {
 
 const functionExpression = function (a, b, ...rest) { return null; };
 
-const functionObject = new Function ('a', 'b', 'return null');
+const functionObject = new Function('a', 'b', 'return null');
 
 const arrowFunction = (a, b, ...rest) => null;
 
 // Task #2
-function takeMyParametersAmount() {
-  let i = 0;
-  for (let arg of arguments)
-    i += 1;
-  console.log(i);
-  return null;
+function takeMyParametersAmount(a, b, c) {
+  return console.log(takeMyParametersAmount.length);
 }
-takeMyParametersAmount('a','b','c');
 
 // Task #3
-function logPassedParams() {
-  return arguments.length;
+function logPassedParams(...theArgs) {
+  return console.log(theArgs);
 }
-
-let count = logPassedParams(1, 2, 3);
-console.log(count);
+logPassedParams(1, 2, 3);
+console.log(logPassedParams.length);
 
 // Task #4
-(function () { })();
+
+(function () {
+  alert('An anonymous function');
+})();
