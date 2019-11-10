3cab27d2d8dc5a3eebd9d0031578ff49c8f62986
Ekaterina Makhachek <maek@ciklum.com>
2019-11-10 17:33:20 +0200

    Fixed "return result from func" for all tasks

--- Exercises/arrays-methods/arrays-methods.js
+++ Exercises/arrays-methods/arrays-methods.js
@@ -1,29 +1,24 @@
 // Task #1
 const array = [1, 2, 3, 4];
-function isElementPresent(ar, element) {
-    return console.log(ar.includes(element));
-}
-isElementPresent(array, 5);
+const isElementPresent = (ar, element) => ar.includes(element);
+console.log(isElementPresent(array, 3));
 
 // Task #2
 const array = [2, 4];
-function isArrayEven(ar) {
-    return console.log(Boolean(!ar.filter(item => item % 2 === 1).length));
-}
-isArrayEven(array);
+const isArrayEven = (ar) => Boolean(!ar.filter(item => item % 2 === 1).length);
+console.log(isArrayEven(array));
 
 // Task #3
 const array = [];
-function isArray(ar) {
-    return console.log(Array.isArray(ar));
-}
-isArray(array);
+const isArray = (ar) => Array.isArray(ar);
+console.log(isArray(array));
 
 // Task #4
-const array = [1, 2, 3, 4];
-function insertElemToArrayByIndex(ar, indexToInsert, element) {
-    ar[indexToInsert] = element;
+// Task #4
+const array = [1, 2, 3, 4]
+function insertElemToArrayByIndexNotPure(ar, indexToInsert, element) {
+    ar.splice(indexToInsert, 0, element);
     return ar;
 }
-insertElemToArrayByIndex(array, 5, 100);
+insertElemToArrayByIndexNotPure(array, 1, 100);
 console.log(array);
