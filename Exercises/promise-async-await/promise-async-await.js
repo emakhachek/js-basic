0abdc99a7e802aad1a89fea0325772bfa505f5a2
Ekaterina Makhachek <maek@ciklum.com>
2020-01-19 16:11:54 +0200

    promise-async-await task is completed

--- Exercises/promise-async-await/promise-async-await.js
+++ Exercises/promise-async-await/promise-async-await.js
@@ -1,7 +1,47 @@
-const fakeAjax = async () => {
-  if (Math.random() > 0.5) {
-    return { status: 200 };
-  }
+const wait = time => new Promise((resolve) => setTimeout(resolve, time));
 
-  throw new Error('Server error!');
+const fakeAjax = async () => {
+    return new Promise((resolve, reject) => {
+        if (Math.random() > 0.1) {
+            resolve(console.log('200'))
+        } else {
+            reject(new Error('Server error'));
+        }
+    });
 };
+
+(async () => {
+    try {
+        await Promise.all([
+            fakeAjax().catch(err => {
+                console.log(err);
+                return null;
+            }),
+            fakeAjax().catch(err => {
+                console.log(err);
+                return null;
+            }),
+            fakeAjax().catch(err => {
+                console.log(err);
+                return null;
+            })
+        ]);
+    } catch (err) {
+        console.log(err);
+    }
+})();
+
+console.log('-----------');
+
+(async () => {
+    let i = 0;
+    while (i < 3) {
+        try {
+            await fakeAjax();
+            await wait(1000);
+        } catch (err) {
+            console.log(err);
+        }
+        i++;
+    }
+})();
