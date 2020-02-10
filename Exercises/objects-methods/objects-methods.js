ddfc113ef62cebf33423631b79cfb057f32679ae
emakhachek <kmahachek@gmail.com>
2020-02-10 12:01:31 +0200

    Objects-methods task is completed

--- Exercises/objects-methods/objects-methods.js
+++ Exercises/objects-methods/objects-methods.js
@@ -1,1 +1,10 @@
-test
+const testObj = {
+    a: 1,
+    b: 2,
+};
+function a(obj) {
+    Object.getOwnPropertyNames(obj).forEach(function (element) {
+        console.log(`${element} : ${obj[element]}`);
+    });
+}
+a(testObj);
